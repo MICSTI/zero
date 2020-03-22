d33311f8be81f7eec731c0b62e90648d828af926
MICSTI <stifter.michael@gmx.net>
2020-03-22 17:05:57 +0100

    feat: add global app prefix

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,15 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
   const configService = app.get(ConfigService);
   const port = configService.get('APP_PORT');
 
   await app.listen(port);
+  console.log(
+    `🚀  Server listening at http://localhost:${port}/${globalPrefix} 🛡️`,
+  );
 }
 bootstrap();
